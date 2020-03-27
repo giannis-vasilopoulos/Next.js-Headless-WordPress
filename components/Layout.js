3e3cdc8f96eb26c1430a25c2ba7fcfb11e6182ad
Giannis Vasilopoulos <giannisvas123@gmail.com>
2020-03-27 18:03:24 +0200

    add pwa support

--- components/Layout.js
+++ components/Layout.js
@@ -11,9 +11,32 @@ const Layout = ({ children, title = "Home", meta }) => {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
         <title>{`${title}`}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+
+        {/*'TODO loop meta data'*/}
+
+        <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link>
+        <meta name="theme-color" content="#303030" />
       </Head>
       <main style={layoutStyle}>{children}</main>
     </>
--- next.config.js
+++ next.config.js
@@ -1,48 +1,53 @@
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
+const withPWA = require("next-pwa");
 const tailwindCss = require("tailwindcss");
-module.exports = withSass({
-  poweredByHeader: false,
-  env: {
-    CMS_URL: process.env.CMS_URL,
-    DEFAULT_LANG: process.env.DEFAULT_LANG
-  },
-  cssModules: false,
-  webpack(config, options) {
-    const rules = [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: [tailwindCss("./tailwind.config.js")]
-            }
-          },
-          { loader: "sass-loader" }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "react-svg-loader",
-            options: {
-              svgo: {
-                plugins: [{ removeViewBox: false }]
+module.exports = withPWA(
+  withSass({
+    pwa: {
+      dest: "public"
+    },
+    poweredByHeader: false,
+    env: {
+      CMS_URL: process.env.CMS_URL,
+      DEFAULT_LANG: process.env.DEFAULT_LANG
+    },
+    webpack(config, options) {
+      const rules = [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: [tailwindCss("./tailwind.config.js")]
+              }
+            },
+            { loader: "sass-loader" }
+          ]
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "react-svg-loader",
+              options: {
+                svgo: {
+                  plugins: [{ removeViewBox: false }]
+                }
               }
             }
-          }
-        ]
-      }
-    ];
-    return {
-      ...config,
-      module: {
-        ...config.module,
-        rules: [...config.module.rules, ...rules]
-      }
-    };
-  }
-});
+          ]
+        }
+      ];
+      return {
+        ...config,
+        module: {
+          ...config.module,
+          rules: [...config.module.rules, ...rules]
+        }
+      };
+    }
+  })
+);
