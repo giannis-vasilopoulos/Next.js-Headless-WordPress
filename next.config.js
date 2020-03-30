4c1c698f407c84c1ea9c47d7d6bf11411ea36941
Giannis Vasilopoulos <giannisvas123@gmail.com>
2020-03-30 15:35:24 +0300

    refactor next.config, add next-compose-plugins package

--- next.config.js
+++ next.config.js
@@ -1,53 +1,73 @@
 require("dotenv").config();
-const withSass = require("@zeit/next-sass");
+const withPlugins = require("next-compose-plugins");
+
+const sass = require("@zeit/next-sass");
 const withPWA = require("next-pwa");
 const tailwindCss = require("tailwindcss");
-module.exports = withPWA(
-  withSass({
-    pwa: {
-      dest: "public"
-    },
-    poweredByHeader: false,
-    env: {
-      CMS_URL: process.env.CMS_URL,
-      DEFAULT_LANG: process.env.DEFAULT_LANG
-    },
-    webpack(config, options) {
-      const rules = [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: "postcss-loader",
-              options: {
-                ident: "postcss",
-                plugins: [tailwindCss("./tailwind.config.js")]
-              }
-            },
-            { loader: "sass-loader" }
-          ]
-        },
-        {
-          test: /\.svg$/,
-          use: [
-            {
-              loader: "react-svg-loader",
-              options: {
-                svgo: {
-                  plugins: [{ removeViewBox: false }]
-                }
+const optimizedImages = require("next-optimized-images");
+
+const nextConfig = {
+  poweredByHeader: false,
+  env: {
+    CMS_URL: process.env.CMS_URL,
+    DEFAULT_LANG: process.env.DEFAULT_LANG
+  },
+  webpack(config, options) {
+    const rules = [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [tailwindCss("./tailwind.config.js")]
+            }
+          },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "react-svg-loader",
+            options: {
+              svgo: {
+                plugins: [{ removeViewBox: false }]
               }
             }
-          ]
+          }
+        ]
+      }
+    ];
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, ...rules]
+      }
+    };
+  }
+};
+
+module.exports = withPlugins(
+  [
+    // [
+    //   optimizedImages,
+    //   {
+    //     /* config for next-optimized-images */
+    //   }
+    // ],
+    [sass],
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public"
         }
-      ];
-      return {
-        ...config,
-        module: {
-          ...config.module,
-          rules: [...config.module.rules, ...rules]
-        }
-      };
-    }
-  })
+      }
+    ]
+  ],
+  nextConfig
 );
