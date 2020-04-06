7b7c828aa50528b02a4e6cb5c6fb81898b073115
Giannis Vasilopoulos <giannisvas123@gmail.com>
2020-04-06 13:03:59 +0300

    add query string package

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { LocaleContext } from "../context/LocaleContext";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { LocaleContext } from '../context/LocaleContext';
 class NextDocument extends Document {
-  static contextType = LocaleContext;
+	static contextType = LocaleContext;
 
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-  render() {
-    const { locale } = this.context;
-    return (
-      <Html lang={locale}>
-        <Head />
-        <body className={`next-${locale} container`}>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		const { locale } = this.context;
+		return (
+			<Html lang={locale}>
+				<Head />
+				<body className={`next-${locale} container`}>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default NextDocument;
